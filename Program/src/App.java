4883ce86547a4a4b84e839c243a2ac8799682cd7
Cstan <tancs8803@gmail.com>
2024-07-20 12:58:45 +0800

    Auto stash before merge of "main" and "origin/main"

--- Program/src/App.java
+++ Program/src/App.java
@@ -1,8 +1,11 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+<<<<<<< Updated upstream
         ///Helllo wotld
         //Hello macibai
         //hrllo
+=======
+>>>>>>> Stashed changes
     }
 }
