69884ae8f7e4650a01e85c39259eebb7debce515
Cstan <tancs8803@gmail.com>
2024-07-20 12:56:32 +0800

    Testing 2.0

--- Program/src/App.java
+++ Program/src/App.java
@@ -2,5 +2,6 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
         ///Helllo wotld
+        //Hello macibai
     }
 }
