0f1a336a800cf1666ed773e9abd40dd0991db374
Wa1ton Gan <gancc-wm23@student.tarc.edu.my>
2024-09-06 16:57:01 +0800

    2nd pushed

--- inventorysystem/src/main/java/project/modules/user/User.java
+++ inventorysystem/src/main/java/project/modules/user/User.java
@@ -13,6 +13,7 @@ import project.global.SqlConnector;
 import project.global.SystemRunNo;
 
 public abstract class User {
+    //private data field can be only access by the class itself
     private final SqlConnector Connector = new SqlConnector();
     private static Scanner scanner = new Scanner(System.in);
     private String userId;
@@ -272,13 +273,11 @@ public abstract class User {
                         System.out.println("Invalid ID or password.");
                         System.out.println("You have " + (MAX_ATTEMPTS - attempts - 1) + " attempts left.");
                         attempts++;
-                        continue;
-                    }
-    
-                    User users = this.userType.equals("Admin") ? new Admin() : new Retailer();
 
-    
-                    if (users != null) {
+                        // Fetch Email and Reg Date for OTP if password fails
+                        try (PreparedStatement statement1 = conn.prepareStatement(sql1)) {
+                            statement1.setString(1, this.getUserId());
+                            ResultSet resultSet1 = statement1.executeQuery();
 
                             if (resultSet1.next()) {
                                 this.setUserEmail(resultSet1.getString(this.getUserType() + "_Email"));
@@ -287,8 +286,7 @@ public abstract class User {
                             }
                         }
 
-                        break;
-
+                        continue; // Go for next login attempt
                     }
 
                     if (this.userType.equals("Retailer")) {
@@ -312,9 +310,12 @@ public abstract class User {
                     setLoggedInUserId(this.userId);  // Set the logged-in user's ID
                     return true;  // Exit loop on successful login
                 }
+
+            } catch (SQLException e) {
+                System.out.println("SQL Error: " + e.getMessage());
             }
 
-        
+        }
 
         // Case 3: Maximum attempts reached
         if (attempts >= MAX_ATTEMPTS) {
@@ -437,10 +438,9 @@ public abstract class User {
 
         System.out.println("Enter the OTP sent to your email: ");
         String userOTP = scanner.nextLine();
-        scanner.close();
 
     
-        String sql= "SELECT otp_expiry, otp_code FROM " + this.getUserType() + " WHERE " + this.getUserType() + "_Id = ?";
+        String sql = "SELECT otp_expiry, otp_code, " + this.getUserType() + "_Password FROM " + this.getUserType() + " WHERE " + this.getUserType() + "_Id = ?";
 
         try (Connection conn = DriverManager.getConnection(url, user, password)) {
             if (conn == null || conn.isClosed()) {
@@ -457,6 +457,7 @@ public abstract class User {
                 if (resultSet.next()) {
                     this.otpExpiry = resultSet.getTimestamp(1).toLocalDateTime();
                     this.otpCode = resultSet.getString(2);
+                    this.setUserPassword(resultSet.getString(3)); 
                 }
 
             } catch (SQLException e) {
@@ -499,6 +500,7 @@ public abstract class User {
             new MailTemplate(this.otpCode, MailTemplate.TemplateType.OTP));
         mail.Send();
     }
+
 
     public void displayUserDetails() {  // can work
         System.out.println(this.getUserType()+" ID: " + this.getUserId());
